db881b814e17772507993a8091e09c04e76edd16
GoKo-Son626 <wangshuqi626@126.com>
2024-07-12 16:33:26 +0800

    [Study]Have learned `Capacitive touch button` and completed experiment of it

---
+++ 入门篇/7.电容触摸按键/电容触摸按键 实验/User/main.c
@@ -0,0 +1,56 @@
+/**
+ ****************************************************************************************************
+ * @file        main.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2020-04-20
+ * @brief       跑马灯 实验
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 STM32F103开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ ****************************************************************************************************
+ */
+
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/delay/delay.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./BSP/LED/led.h"
+#include "./BSP/TPAD/tpad.h"
+
+
+int main(void)
+{
+    uint16_t t = 0;
+    
+    HAL_Init();                                 /* 初始化HAL库 */
+    sys_stm32_clock_init(RCC_PLL_MUL9);         /* 设置时钟,72M */
+    delay_init(72);                             /* 初始化延时函数 */
+    usart_init(115200);                         // 串口初始化
+    led_init();                                 /* 初始化LED */
+    tpad_init(6);                               // 初始化触摸按键
+    
+    while(1)
+    {
+        if (tpad_scan(0))   /* 成功捕获到了一次上升沿(此函数执行时间至少15ms) */
+        {
+            LED1_TOGGLE();  /* LED1翻转 */
+        }
+
+        t++;
+        if (t == 15)
+        {
+            t = 0;
+            LED0_TOGGLE();  /* LED0翻转 */
+        }
+
+        delay_ms(10);
+    }
+}
+
